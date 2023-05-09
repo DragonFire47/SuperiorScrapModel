fc8dac59354435421a77fea76df476088f6b1081
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2023-05-08 18:16:43 -0700

    Add project files.

---
+++ SuperiorScrapModel/Mod.cs
@@ -0,0 +1,29 @@
+﻿using PulsarModLoader;
+using System.Reflection;
+using UnityEngine;
+
+namespace SuperiorScrapModel
+{
+    public class Mod : PulsarMod
+    {
+        static AssetBundle AssetBundle = null;
+        public static GameObject ScrapAsset = null;
+
+        public Mod()
+        {
+            AssetBundle = AssetBundle.LoadFromStream(Assembly.GetExecutingAssembly().GetManifestResourceStream("SuperiorScrapModel.scrapmodel.scrap"));
+            ScrapAsset = AssetBundle.LoadAsset<GameObject>("Assets/PrefabInstance/waste.prefab");
+        }
+
+        public override string Version => "1.0.0";
+
+        public override string Author => "Dragon";
+
+        public override string Name => "SuperiorScrapModel";
+
+        public override string HarmonyIdentifier()
+        {
+            return $"{Author}.{Name}";
+        }
+    }
+}
---
+++ SuperiorScrapModel/Patches.cs
@@ -0,0 +1,49 @@
+﻿using HarmonyLib;
+using System;
+using UnityEngine;
+
+namespace SuperiorScrapModel
+{
+    [HarmonyPatch(typeof(PLScrapCargo), MethodType.Constructor, new Type[] { typeof(int) })]
+    internal class ScrapPatch
+    {
+        static void Postfix(PLScrapCargo __instance)
+        {
+            __instance.CargoVisualPrefabID = 10000;
+        }
+    }
+    [HarmonyPatch(typeof(PLShipInfo), "SetupCOD")]
+    class ShipSetupCODPatch
+    {
+        static bool Prefix(PLShipInfo __instance, CargoObjectDisplay cod, PLShipComponent shipComp, bool hidden)
+        {
+            if(shipComp.CargoVisualPrefabID != 10000)
+            {
+                return true;
+            }
+            if (shipComp != null)
+            {
+                AccessTools.Method(typeof(PLShipInfo), "TeleportNearbyPlayers").Invoke(__instance, new object[] { cod.RootObj.transform });
+            }
+            if (cod.DisplayedItem != shipComp)
+            {
+                UnityEngine.Object.Destroy(cod.DisplayObj);
+                cod.DisplayObj = null;
+            }
+            cod.DisplayedItem = shipComp;
+            cod.DisplayObj = UnityEngine.Object.Instantiate<GameObject>(Mod.ScrapAsset, cod.RootObj.transform.position, cod.RootObj.transform.rotation);
+            cod.DisplayObj.layer = 11;
+            foreach (object obj in cod.DisplayObj.transform)
+            {
+                ((Transform)obj).gameObject.layer = 11;
+            }
+            cod.DisplayObj.transform.parent = cod.RootObj.transform;
+            cod.DisplayObj.transform.localPosition = Vector3.zero;
+            cod.DisplayObj.transform.localScale = Vector3.one;
+            cod.DisplayObj.transform.localRotation = Quaternion.identity;
+            cod.DisplayObjRenderers = cod.DisplayObj.GetComponentsInChildren<Renderer>(true);
+            cod.RootObjRenderer = cod.RootObj.GetComponent<Renderer>();
+            return false;
+        }
+    }
+}
